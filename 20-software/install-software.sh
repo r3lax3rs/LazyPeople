fb50fc8a07e6c4b10e60162119241bb944ac5f64
r3lax3rs <124855911+r3lax3rs@users.noreply.github.com>
2025-04-18 19:36:13 +0200

    Update install-software.sh

--- 20-software/install-software.sh
+++ 20-software/install-software.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+install_software() {
+    case "$1" in
+        arch)
+            read -p "Enter package to install: " pkg
+            sudo pacman -S "$pkg" --needed --noconfirm
+            ;;
+        ubuntu)
+            read -p "Enter package to install: " pkg
+            sudo apt install "$pkg" -y
+            ;;
+        rocky|centos)
+            read -p "Enter package to install: " pkg
+            sudo dnf install "$pkg" -y
+            ;;
+        *)
+            echo "Unsupported distro: $1"
+            ;;
+    esac
+    pause
+}
