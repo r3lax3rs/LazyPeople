71696a6f73136a26a04370c2eb2f49dc1f56ce12
r3lax3rs <124855911+r3lax3rs@users.noreply.github.com>
2025-04-18 19:34:59 +0200

    Update update-system.sh

--- 10-system/update-system.sh
+++ 10-system/update-system.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+update_system() {
+    case "$1" in
+        arch)
+            sudo pacman -Syu
+            ;;
+        ubuntu)
+            sudo apt update && sudo apt upgrade -y
+            ;;
+        rocky|centos)
+            sudo dnf update -y
+            ;;
+        *)
+            echo "Unsupported distro: $1"
+            ;;
+    esac
+    pause
+}
