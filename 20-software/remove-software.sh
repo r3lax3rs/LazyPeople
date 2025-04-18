e0fc845619f8b8dc42041a5fd6462909323c082d
r3lax3rs <124855911+r3lax3rs@users.noreply.github.com>
2025-04-18 19:36:31 +0200

    Update remove-software.sh

--- 20-software/remove-software.sh
+++ 20-software/remove-software.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+remove_software() {
+    case "$1" in
+        arch)
+            read -p "Enter package to remove: " pkg
+            sudo pacman -Rns "$pkg"
+            ;;
+        ubuntu)
+            read -p "Enter package to remove: " pkg
+            sudo apt remove "$pkg" -y
+            ;;
+        rocky|centos)
+            read -p "Enter package to remove: " pkg
+            sudo dnf remove "$pkg" -y
+            ;;
+        *)
+            echo "Unsupported distro: $1"
+            ;;
+    esac
+    pause
+}
