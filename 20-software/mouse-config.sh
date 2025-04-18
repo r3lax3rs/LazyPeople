2aa7db5f9098a881823fd98759e3dc422d01d99a
r3lax3rs <124855911+r3lax3rs@users.noreply.github.com>
2025-04-18 19:36:56 +0200

    Update mouse-config.sh

--- 20-software/mouse-config.sh
+++ 20-software/mouse-config.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+mouse_settings() {
+    echo "1. Enable acceleration"
+    echo "2. Disable acceleration"
+    read -p "Choose option: " opt
+    case "$opt" in
+        1)
+            xinput --set-prop "pointer:Logitech USB Optical Mouse" "libinput Accel Profile Enabled" 0, 1
+            ;;
+        2)
+            xinput --set-prop "pointer:Logitech USB Optical Mouse" "libinput Accel Profile Enabled" 1, 0
+            ;;
+        *)
+            echo "Invalid option"
+            ;;
+    esac
+    pause
+}
